4952022780c6fce5b998b73289834c185a6a688b
Mark Huang <hhzeos@gmail.com>
2021-04-03 15:54:38 +0800

    Update

--- blur_single_video.py
+++ blur_single_video.py
@@ -2,12 +2,14 @@ from pathlib import Path
 
 import cv2
 import face_recognition
+from markkk.logger import logger
 
 
 def check_single_video(video_filepath: str, batch_size: int = 3) -> bool:
     assert Path(video_filepath).is_file()
     # Open video file
     video_capture = cv2.VideoCapture(video_filepath)
+    logger.debug(f"Checking faces in: {video_filepath}")
 
     frames = []
     frame_count = 0
@@ -43,19 +45,19 @@ def check_single_video(video_filepath: str, batch_size: int = 3) -> bool:
                 if number_of_faces_in_frame >= 1:
                     return True
 
-                frame_number = frame_count - BATCH_SIZE + frame_number_in_batch
-                print(
-                    f"Found {number_of_faces_in_frame} face(s) in frame #{frame_number}."
-                )
+                # frame_number = frame_count - BATCH_SIZE + frame_number_in_batch
+                # print(
+                #     f"Found {number_of_faces_in_frame} face(s) in frame #{frame_number}."
+                # )
 
-                for face_location in face_locations:
-                    # Print the location of each face in this frame
-                    top, right, bottom, left = face_location
-                    print(
-                        " - A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(
-                            top, left, bottom, right
-                        )
-                    )
+                # for face_location in face_locations:
+                #     # Print the location of each face in this frame
+                #     top, right, bottom, left = face_location
+                #     print(
+                #         " - A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(
+                #             top, left, bottom, right
+                #         )
+                #     )
 
             # Clear the frames array to start the next batch
             frames = []
